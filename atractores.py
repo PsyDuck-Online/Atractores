9a0abb9a0ef18f443a7db311afa21ab4df1d18a0
PsyDuck <43123963+PsyDuck-Online@users.noreply.github.com>
2023-01-11 19:49:08 -0600

    Barra de carga implementada

--- atractores.py
+++ atractores.py
@@ -1,5 +1,6 @@
 import numpy as np
 from life import *
+from progress.bar import ChargingBar
 import sys
 
 
@@ -9,24 +10,26 @@ def main():
         N = int(sys.argv[2])
     else:
         return
+
     f = open(nombre_archivo+"_nodos.csv", 'w')
     f2 = open(nombre_archivo+"_aristas.csv", 'w')
     f.write("ID,Label\n")
     f2.write("Source,Target\n")
 
     combinaciones = 2**(N*N)  # Total de combinaciones en el tablero
+    bar = ChargingBar('Calculando Atractores:', max=combinaciones)
     # Calcula la funcion de transicion
     for estado in range(0, combinaciones):
         tablero = numToMatriz(estado, N)
         nextGen(tablero)
         estadoSig = matrizToNum(tablero, N)
-        # tuplas.append((estado,estadoSig))
         f.write("{},{}\n".format(estado, estado))
         f2.write("{},{}\n".format(estado, estadoSig))
-
-    # for tupla in tuplas:
-     #   f.write("{} {}\n".format(tupla[0], tupla[1]))
+        bar.next()
     f.close()
+    f2.close()
+
+    print("")
 
 
 def estaEnLista(lista, val):
